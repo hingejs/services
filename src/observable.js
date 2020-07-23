8341b8c05f0758e3cf5f2f767bc1babc6814614b
gforti <gforti@its.jnj.com>
2020-07-23 16:27:19 -0400

    refactor: observable upgrades, allow for limit

--- src/observable.js
+++ src/observable.js
@@ -1,49 +1,96 @@
 export default class Observable {
 
   constructor() {
+    this.reset()
+  }
+
+  reset() {
     this.observers = new Map()
     this.errors = new Map()
     this.completes = new Map()
+    this.counter = new Map()
+    this.maxLife = new Map()
+  }
+
+  isFunction(f) {
+    return typeof f === 'function'
   }
 
   subscribe(f) {
     let complete, error, next = f
-    if (typeof f !== 'function') {
+    if (!this.isFunction(f)) {
       ({ next, error, complete } = f)
     }
     const uid = new Date().getTime().toString(36) + performance.now().toString().replace(/[^0-9]/g, '')
-    if (next) {
-      this.observers.set(uid, next)
+    if (this.isFunction(next)) {
+      const nextModified = (subject) => {
+        const count = ~~this.counter.get(uid) + 1
+        this.counter.set(uid, count)
+        next(subject)
+        this.only(uid)
+      }
+      this.observers.set(uid, nextModified)
+      this.counter.set(uid, 0)
     }
-    if (error) {
+    if (this.isFunction(error)) {
       this.errors.set(uid, error)
     }
-    if (complete) {
+    if (this.isFunction(complete)) {
       this.completes.set(uid, complete)
     }
-    return { uid, unsubscribe: this.unsubscribe.bind(this, uid) }
+    const methods = {
+      only: (times) => {
+        this.only(uid, times)
+        return methods
+      },
+      uid,
+      unsubscribe: this.unsubscribe.bind(this, uid),
+    }
+    return methods
   }
 
   unsubscribe(uid) {
+    const complete = this.completes.get(uid)
+    if (this.isFunction(complete)) {
+      complete()
+    }
     this.observers.delete(uid)
     this.errors.delete(uid)
+    this.completes.delete(uid)
+    this.counter.delete(uid)
+    this.maxLife.delete(uid)
   }
 
   complete() {
     this.completes.forEach(observer => observer())
-    this.observers = new Map()
-    this.errors = new Map()
-    this.completes = new Map()
+    this.reset()
   }
 
   notify() {
-    const args = Array.from(arguments)
-    this.observers.forEach(observer => observer.apply(null, args))
+    const subject = Array.from(arguments)
+    this.observers.forEach(observer => observer.apply(null, subject))
   }
 
   notifyError() {
-    const args = Array.from(arguments)
-    this.errors.forEach(observer => observer.apply(null, args))
+    const subject = Array.from(arguments)
+    this.errors.forEach(observer => observer.apply(null, subject))
   }
 
+  /**
+   * The maximum number of times to notify
+   * @param {string} uid - subscription id
+   * @param {number} times - max number, must be greater than zero(0)
+   */
+  only(uid, times) {
+    if (!isNaN(times) && times > 0) {
+      this.maxLife.set(uid, times)
+    }
+    if (this.maxLife.has(uid)) {
+      const maxCount = ~~this.maxLife.get(uid)
+      const count = ~~this.counter.get(uid)
+      if (maxCount > 0 && count >= maxCount) {
+        this.unsubscribe(uid)
+      }
+    }
+  }
 }
