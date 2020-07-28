b4f7535af323259396971fa0db9613e50dbec6d1
gforti <gforti@its.jnj.com>
2020-07-28 16:40:56 -0400

    refactor: fix mixin

--- src/model.mixin.js
+++ src/model.mixin.js
@@ -1,28 +1,27 @@
 import HtmlMarker from './html-marker.js'
 export default Base => class extends Base {
 
-    constructor() {
-      super()
-      this.model = this.defaultModel
-      this.htmlMarker = new HtmlMarker(this.model)
-    }
-
-    get defaultModel() {
-      return {}
-    }
-
-    updateModel(obj = {}) {
-      const lastModelUpdates = {}
-      Object.entries(obj).forEach(([prop, newValue]) => {
-        if (this.model[prop] !== newValue) {
-          lastModelUpdates[prop] = newValue
-        }
-      })
-      Object.assign(this.model, obj)
-      this.onModelUpdate(lastModelUpdates)
-    }
-
-    onModelUpdate() { }
-
+  constructor() {
+    super()
+    this.model = this.defaultModel
+    this.htmlMarker = new HtmlMarker(this.model)
   }
+
+  get defaultModel() {
+    return {}
+  }
+
+  updateModel(obj = {}) {
+    const lastModelUpdates = {}
+    Object.entries(obj).forEach(([prop, newValue]) => {
+      if (this.model[prop] !== newValue) {
+        lastModelUpdates[prop] = newValue
+      }
+    })
+    Object.assign(this.model, obj)
+    this.onModelUpdate(lastModelUpdates)
+  }
+
+  onModelUpdate() { }
+
 }
